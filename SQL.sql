cf3d1e1581ea51d0136c50cf0db66c1de34c778f
arsyahendratno <103812197+arsyahendratno@users.noreply.github.com>
2023-09-26 19:58:58 +0700

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,13 +1,11 @@
 -- Test_SQL_1
-WITH consecutive_counts AS (
+SELECT DISTINCT num
+FROM (
     SELECT
         num, id, ROW_NUMBER() OVER (ORDER BY id) - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS identifier
     FROM
         input_table
-)
-
-SELECT DISTINCT num
-FROM consecutive_counts
+) consecutive_counts
 GROUP BY num, identifier
 HAVING COUNT(*) >= 3
 ORDER BY num;
